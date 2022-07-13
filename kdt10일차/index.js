3dfb445633e87050372e42d2f434b74eebae6d90
BogusD <qazxwd44@hanmail.net>
2022-07-13 14:59:56 +0900

    educate(07.13)

---
+++ kdt10일차/index.js
@@ -0,0 +1,65 @@
+// console.log(typeof null);
+// console.log(typeof "nubmer");
+// console.log(typeof true);
+
+
+// console.log(typeof 123, "isn't", typeof "gkgk", "data type");
+// // '
+// let mathScore = "77";
+// let engScore  = "88";
+
+// mathScore= Number(mathScore);
+// engScore=Number(engScore);
+// let avgScore = (mathScore + engScore) /2;
+// console.log(avgScore);
+
+// let nameArray = ["강유림" , "강지훈" , "master" , "김윤비"];
+// console.log(nameArray.length);
+
+// for (let i =0; i <nameArray.length; i++){
+//   console.log(`${i+1} 번째 참가자의 이름은 ${nameArray[i]} 입니다.`);
+// }
+
+
+// let sq =2 ** 2;
+// console.log(sq);
+
+// let num= Math.sqrt(sq);
+// console.log(num);
+
+
+// let num = 10;
+
+// num = num +5;
+// console.log(num);
+
+// num += 5;
+// console.log(num);
+
+// num -= 5;
+// console.log(num);
+
+// num *=5;
+// console.log(num);
+
+// num /=5;
+// console.log(num);
+
+// let num=10;
+
+// console.log(num);
+
+// let a =1;
+// let b = "1";
+
+// console.log(a===b);
+
+let name = "뽀로로";
+let age = 16;
+let isAudlt = age >19;
+
+if(isAudlt){
+  console.log("통과");
+} else{
+  console.log("돌아가");
+}
